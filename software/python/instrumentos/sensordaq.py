2a62e0eb59d42f9fd21d5c196fc26c0687ed272f
Diego Shalom <diegoshalom@gmail.com>
2024-11-13 17:15:17 -0300

    Agrego info sensordaq

--- software/python/instrumentos/sensordaq.py
+++ software/python/instrumentos/sensordaq.py
@@ -14,6 +14,6 @@
 # Input Configuration: RSE
 # Ej: ai_channel = task.ai_channels.add_ai_voltage_chan("Dev1/_sensor0_10V", min_val=-10, max_val=10, terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
 
-# Para acceder a los tres canales, 0ambiar el 0 por 1 o 2.
+# Para acceder a los tres canales, cambiar el 0 por 1 o 2.
 
 # Además, la placa SensorDaq tiene dos canales analógicos ai0 y ai1, en la bornera.
