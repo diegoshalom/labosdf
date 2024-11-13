3c18bf360fa4c030abc81773088c6ed7d176202c
Diego Shalom <diegoshalom@gmail.com>
2024-11-13 16:34:34 -0300

    Agrego info de SensorDaq

---
+++ software/python/instrumentos/sensordaq.py
@@ -0,0 +1,17 @@
+# https://www.vernier.com/files/manuals/sdaq.pdf
+# Todo está en este manual
+
+# Para acceder a los tres canales de sensores, se inicializa igual que una daq, y hay que darle el nombre del canal:
+# Channel Name: "Dev1/_sensor0_5V"
+# Max Input Limit: 5 Volt
+# Min Input Limit: 0 Volt
+# Input Configuration: RSE
+
+# Channel Name: "Dev1/_sensor0_10V"
+# Max Input Limit: 10 Volt
+# Min Input Limit: -10 Volt
+# Input Configuration: RSE
+
+# Para acceder a los tres canales, 0ambiar el 0 por 1 o 2.
+
+# Además, la placa SensorDaq tiene dos canales analógicos ai0 y ai1, en la bornera.
